7f06d3eabdfbec2d1f7fd3ea5aa484f535feca6b
Ezequiel Antonio Nuñez <development.klubit@gmail.com>
2026-02-13 06:43:24 +0100

    feat: add new change

---
+++ src/hooks/useScrollLock.ts
@@ -0,0 +1,33 @@
+import { useEffect, useRef } from 'react';
+
+const useScrollLock = (isLocked: boolean) => {
+    const scrollYRef = useRef(0);
+
+    useEffect(() => {
+        if (!isLocked) return;
+
+        scrollYRef.current = window.scrollY;
+        const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+
+        document.body.style.position = 'fixed';
+        document.body.style.top = `-${scrollYRef.current}px`;
+        document.body.style.left = '0';
+        document.body.style.right = '0';
+        document.body.style.overflow = 'hidden';
+        if (scrollbarWidth > 0) {
+            document.body.style.paddingRight = `${scrollbarWidth}px`;
+        }
+
+        return () => {
+            document.body.style.position = '';
+            document.body.style.top = '';
+            document.body.style.left = '';
+            document.body.style.right = '';
+            document.body.style.overflow = '';
+            document.body.style.paddingRight = '';
+            window.scrollTo(0, scrollYRef.current);
+        };
+    }, [isLocked]);
+};
+
+export default useScrollLock;
